8116c2b661d64798ea0e60c64f0b113839809776
Himanshu Rai <himanshu2003rai@gmail.com>
2024-09-21 15:00:01 +0530

    join and leave subreddit

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,12 +1,12 @@
 "use client";
 
-import { formatTimeToNow } from "@/lib/utils";
+// import { formatTimeToNow } from "@/lib/utils";
 import { Post, User, Vote } from "@prisma/client";
 import { MessageSquare } from "lucide-react";
 import Link from "next/link";
 import { FC, useRef } from "react";
-import EditorOutput from "./EditorOutput";
-import PostVoteClient from "./post-vote/PostVoteClient";
+// import EditorOutput from "./EditorOutput";
+// import PostVoteClient from "./post-vote/PostVoteClient";
 
 type PartialVote = Pick<Vote, "type">;
 
@@ -33,11 +33,11 @@ const Post: FC<PostProps> = ({
   return (
     <div className="rounded-md bg-white shadow">
       <div className="px-6 py-4 flex justify-between">
-        <PostVoteClient
+        {/* <PostVoteClient
           postId={post.id}
           initialVotesAmt={_votesAmt}
           initialVote={_currentVote?.type}
-        />
+        /> */}
 
         <div className="w-0 flex-1">
           <div className="max-h-40 mt-1 text-xs text-gray-500">
@@ -53,7 +53,7 @@ const Post: FC<PostProps> = ({
               </>
             ) : null}
             <span>Posted by u/{post.author.username}</span>{" "}
-            {formatTimeToNow(new Date(post.createdAt))}
+            {/* {formatTimeToNow(new Date(post.createdAt))} */}
           </div>
           <a href={`/r/${subredditName}/post/${post.id}`}>
             <h1 className="text-lg font-semibold py-2 leading-6 text-gray-900">
@@ -65,11 +65,11 @@ const Post: FC<PostProps> = ({
             className="relative text-sm max-h-40 w-full overflow-clip"
             ref={pRef}
           >
-            <EditorOutput content={post.content} />
+            {/* <EditorOutput content={post.content} />
             {pRef.current?.clientHeight === 160 ? (
               // blur bottom if content is too long
               <div className="absolute bottom-0 left-0 h-24 w-full bg-gradient-to-t from-white to-transparent"></div>
-            ) : null}
+            ) : null} */}
           </div>
         </div>
       </div>
