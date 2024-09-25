6374408125155e77279c73daf4c5093c0b93076a
Himanshu Rai <himanshu2003rai@gmail.com>
2024-09-26 00:04:13 +0530

    fix image bug

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['uploadthing.com', 'lh3.googleusercontent.com'],
+    domains: ["uploadthing.com", "lh3.googleusercontent.com", "utfs.io"], // Add 'utfs.io'
   },
   experimental: {
-    appDir: true
-  }
-}
+    appDir: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/components/renderers/CustomImageRenderer.tsx
+++ src/components/renderers/CustomImageRenderer.tsx
@@ -3,8 +3,7 @@
 import Image from "next/image";
 
 function CustomImageRenderer({ data }: any) {
-  const src = data.file.url;
-
+  const src: string = data.file.url;
   return (
     <div className="relative w-full min-h-[15rem]">
       <Image alt="image" className="object-contain" fill src={src} />
