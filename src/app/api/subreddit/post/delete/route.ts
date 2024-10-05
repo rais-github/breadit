f954348c0194878512f74191658e5e9fe007a19b
Himanshu Rai <himanshu2003rai@gmail.com>
2024-10-05 16:50:02 +0530

    delete route fixes

--- src/app/api/subreddit/post/delete/route.ts
+++ src/app/api/subreddit/post/delete/route.ts
@@ -2,44 +2,46 @@ import { getAuthSession } from "@/lib/auth";
 import { db } from "@/lib/db";
 import { NextResponse } from "next/server";
 
-// Handling DELETE method
 export async function DELETE(req: Request) {
   const { searchParams } = new URL(req.url);
-  const postId = searchParams.get("postId"); // extract the postId from the URL
+  const postId = searchParams.get("postId");
 
   try {
     const session = await getAuthSession();
 
     if (!session?.user) {
-      return NextResponse.json("Unauthorized", { status: 401 });
+      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
     }
 
-    // Find the post by ID
     const post = await db.post.findUnique({
-      where: {
-        id: postId || "",
-      },
+      where: { id: postId || "" },
     });
 
     if (!post) {
-      return NextResponse.json("Post not found", { status: 404 });
+      return NextResponse.json({ message: "Post not found" }, { status: 404 });
     }
 
     if (post.authorId !== session.user.id) {
-      return NextResponse.json("You are not authorized to delete this post", {
-        status: 403,
-      });
+      return NextResponse.json(
+        { message: "You are not authorized to delete this post" },
+        { status: 403 }
+      );
     }
 
-    // Delete the post
+    // Attempt to delete the post
     await db.post.delete({
-      where: {
-        id: postId || "",
-      },
+      where: { id: postId || "" },
     });
 
-    return NextResponse.json("Post deleted successfully", { status: 200 });
-  } catch (error) {
-    return NextResponse.json("Failed to delete post.", { status: 500 });
+    return NextResponse.json(
+      { message: "Post deleted successfully" },
+      { status: 200 }
+    );
+  } catch (error: any) {
+    console.error("Error deleting post:", error);
+    return NextResponse.json(
+      { message: "Failed to delete post.", error: error.message },
+      { status: 500 }
+    );
   }
 }
